3b9fa534424ab53af26761db8619a2fcdbd64c19
Alexander Fabian <alexander.fabian@hadiko.de>
2022-07-30 11:46:33 +0200

    Add groups to org member display

--- src/shiftings/organizations/forms/membership.py
+++ src/shiftings/organizations/forms/membership.py
@@ -1,21 +1,25 @@
+from typing import Optional
+
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 from shiftings.accounts.models import User
-from shiftings.organizations.models import Membership, Organization
+from shiftings.organizations.models import Membership
 
 
 class MembershipForm(forms.ModelForm):
-    user = forms.CharField(max_length=150, label=_('Username'))
+    user = forms.CharField(max_length=150, label=_('Username'), required=False)
 
     class Meta:
         model = Membership
         fields = ['organization', 'user', 'group']
         widgets = {'organization': forms.HiddenInput()}
 
-    def clean_user(self) -> User:
+    def clean_user(self) -> Optional[User]:
         username = self.cleaned_data.get('user')
+        if not username:
+            return None
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist as e:
--- src/shiftings/static/js/remove_modal.js
+++ src/shiftings/static/js/remove_modal.js
@@ -9,11 +9,15 @@
       let button = event.relatedTarget;
       // Extract info from data-bs-* attributes
 
-      let removeObjectPlaceholder = document.getElementById('remove_object_placeholder');
+      let removeObjectPlaceholder1 = document.getElementById('remove_object_placeholder_1');
+      let removeObjectPlaceholder2 = document.getElementById('remove_object_placeholder_2');
       let removeForm = document.getElementById('id_remove_form');
         
       removeForm.action = button.getAttribute('data-bs-remove-url');
-      removeObjectPlaceholder.textContent = button.getAttribute('data-bs-remove-name');
+      console.log(removeObjectPlaceholder1)
+      console.log(removeObjectPlaceholder2)
+      removeObjectPlaceholder1.textContent = button.getAttribute('data-bs-remove-name');
+      removeObjectPlaceholder2.textContent = button.getAttribute('data-bs-remove-name');
     });
   }
 })();
