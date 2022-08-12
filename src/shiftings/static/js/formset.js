013018ebf97010585252fe837d40a3b548e473d0
Tjeri <tjeri@hadiko.de>
2022-08-12 22:34:12 +0200

    better formset button

--- src/shiftings/static/js/formset.js
+++ src/shiftings/static/js/formset.js
@@ -1,37 +1,35 @@
 'use strict';
 (function () {
   function initialize() {
-    let total = document.getElementById('id_form-TOTAL_FORMS');
-    let totalValue = parseInt(total.value);
-    let max = document.getElementById('id_form-MAX_NUM_FORMS');
-    let maxValue = parseInt(max.value);
-    if (totalValue >= maxValue) {
-      return;
-    }
-    let form = total.parentElement;
-    let insert = document.getElementById('formset_insert');
-    let template = document.getElementById('formset_template');
+    const template = document.getElementById('formset_template');
+    const totalElement = document.getElementById('id_form-TOTAL_FORMS');
+    let totalValue = parseInt(totalElement.value);
+    const maxValue = parseInt(document.getElementById('id_form-MAX_NUM_FORMS').value);
+    const form = document.getElementById('formset_form') || totalElement.parentElement;
+    const insert = document.getElementById('formset_insert') || form;
 
-    let addLink = document.createElement('a');
-    addLink.classList.add('btn', 'btn-secondary');
-    addLink.innerHTML = '<i class="fas fa-plus"></i>';
+    let addLink = document.getElementById('formset_add');
+    if (!addLink) {
+      addLink = document.createElement('a');
+      addLink.classList.add('btn', 'btn-secondary');
+      addLink.innerHTML = '<i class="fas fa-plus"></i>';
+      const submit = document.getElementById('formset_submit') || form.querySelector('[type=submit]');
+      if (submit) {
+        submit.parentNode.insertBefore(addLink, submit);
+      } else {
+        form.insertAdjacentElement('beforeend', addLink);
+      }
+    }
 
     addLink.addEventListener('click', () => {
       let newForm = template.content.cloneNode(true);
       Array.from(newForm.children).forEach(e => e.innerHTML = e.innerHTML.replaceAll('__prefix__', totalValue));
-      total.value = ++totalValue;
+      totalElement.value = ++totalValue;
       insert.appendChild(newForm);
       if (totalValue >= maxValue) {
-        addLink.remove();
+        addLink.classList.add('hidden');
       }
     });
-
-    let submit = form.querySelector('[type=submit]');
-    if (submit) {
-      submit.parentNode.insertBefore(addLink, submit);
-    } else {
-      form.insertAdjacentElement('beforeend', addLink);
-    }
   }
 
   document.addEventListener('DOMContentLoaded', initialize);
