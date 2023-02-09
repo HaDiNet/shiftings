c9a61d46641ed74c745fb15f89f3dbd1361d563d
Tjeri <tjeri@hadiko.de>
2023-02-09 21:50:42 +0100

    remove default shfit type from summary settings form

--- src/shiftings/shifts/forms/summary.py
+++ src/shiftings/shifts/forms/summary.py
@@ -4,17 +4,6 @@ from shiftings.shifts.models import OrganizationSummarySettings
 
 
 class OrganizationShiftSummaryForm(ModelForm):
-    default_shift_types = MultipleChoiceField(widget=CheckboxSelectMultiple, choices=[])
-
     class Meta:
         model = OrganizationSummarySettings
-        fields = ['default_time_range_type', 'default_shift_types']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['default_shift_types'].choices = [type_.choice for type_ in
-                                                      self.instance.organization.shift_types.all()]
-        if self.instance.default_shift_types is not None:
-            default_ids = self.instance.default_shift_types
-            self.fields['default_shift_types'].initial = default_ids if len(default_ids) != 0 else \
-                self.instance.organization.shift_types.values_list('pk', flat=True)
+        fields = ['default_time_range_type']
