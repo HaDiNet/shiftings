ea659f03d606ed9f7303283ad84d9f017a9842da
Alexander Fabian <alexander.fabian@hadiko.de>
2023-02-16 23:05:25 +0100

    Revert removal of OrganizationEditShiftSummarySettingsView

--- src/shiftings/shifts/views/summary.py
+++ src/shiftings/shifts/views/summary.py
@@ -7,8 +7,24 @@ from django.views.generic import DetailView
 
 from shiftings.organizations.models import Organization
 from shiftings.organizations.views.organization_base import OrganizationPermissionMixin
-from shiftings.shifts.forms.summary import SelectSummaryTimeRangeForm
+from shiftings.shifts.forms.summary import OrganizationShiftSummaryForm, SelectSummaryTimeRangeForm
+from shiftings.shifts.models import OrganizationSummarySettings
 from shiftings.utils.time.timerange import TimeRangeType
+from shiftings.utils.views.create_update_view import CreateOrUpdateView
+
+
+class OrganizationEditShiftSummarySettingsView(OrganizationPermissionMixin, CreateOrUpdateView):
+    permission_required = 'organizations.edit_organization'
+    model = OrganizationSummarySettings
+    form_class = OrganizationShiftSummaryForm
+
+    object: OrganizationSummarySettings
+
+    def get_organization(self) -> Organization:
+        return self.get_object().organization
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
 
 
 class OrganizationShiftSummaryView(OrganizationPermissionMixin, DetailView):
