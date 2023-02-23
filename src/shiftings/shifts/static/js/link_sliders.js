a34abe9ea385fcb768aa096ad38e815468ef7bb1
Tjeri <tjeri@hadiko.de>
2023-02-23 22:26:23 +0100

    fix sliders in edit of shift templates
    it was only linked on update
    now I also link them once after page load
    
    it doesn't need to be linked on dom changes, because new sliders start at default value

--- src/shiftings/shifts/static/js/link_sliders.js
+++ src/shiftings/shifts/static/js/link_sliders.js
@@ -7,17 +7,27 @@
     return elements[0];
   }
 
-  document.addEventListener('input', event => {
-    const element = event.target;
-    if (element.name.includes('start_delay')) {
-      const nodes = document.getElementsByName(element.name.replace('start_delay', 'duration'));
-      if (nodes.length !== 1) {
-        throw new Error('Found more or less than one end.');
-      }
-      const end = nodes[0];
-      end.attributes.start.nodeValue = getElement(element, 'time-slider-display').value;
-      end.attributes.startDay.nodeValue = getElement(element, 'time-slider-day').innerText;
-      end.dispatchEvent(new CustomEvent('startChanged', {bubbles: true, target: end}));
+  function updateSliders(slider) {
+    if (!slider.name.includes('start_delay')) {
+      return;
     }
+    const nodes = document.getElementsByName(slider.name.replace('start_delay', 'duration'));
+    if (nodes.length !== 1) {
+      throw new Error('Found more or less than one end.');
+    }
+    const end = nodes[0];
+    end.attributes.start.nodeValue = getElement(slider, 'time-slider-display').value;
+    end.attributes.startDay.nodeValue = getElement(slider, 'time-slider-day').innerText;
+    end.dispatchEvent(new CustomEvent('startChanged', { bubbles: true, target: end }));
+  }
+
+  document.addEventListener('input', event => {
+    updateSliders(event.target);
   });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    for (const slider of document.getElementsByClassName('time-slider')) {
+      updateSliders(slider);
+    }
+  })
 })();
