cdf3fab4dc156ba986165511b51cdc4a80cdaa6e
Tjeri <tjeri92@gmail.com>
2023-12-21 22:20:32 +0100

    save auto create days to recurring shift
    - add auto_create_days as field to recurring shift
    - use old setting (renamed) as default
    - validate min/max
    - use that field in the recurring shift generation
    - add --shift to the generation command to only generate for 1 specific recurring shift
    - make --days only work with --shift

--- src/shiftings/shifts/management/commands/create_recurring_shifts.py
+++ src/shiftings/shifts/management/commands/create_recurring_shifts.py
@@ -1,22 +1,29 @@
 from datetime import date, timedelta
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from shiftings.shifts.models import RecurringShift
 
 
 class Command(BaseCommand):
-    help = f'Creates recurring shifts within the next {settings.RECURRING_SHIFTS_AUTO_CREATE_DAYS} days.'
+    help = f'Creates recurring shifts within the next days as configured within the shifts.'
 
     def add_arguments(self, parser):
-        parser.add_argument('--days', type=int, help='override days to create')
+        parser.add_argument('--shift', type=int, help='create only for the shift with this pk')
+        parser.add_argument('--days', type=int, help='override days to create (only works together with --shift)')
 
     def handle(self, *args, **options):
-        for i in range(options['days'] or settings.RECURRING_SHIFTS_AUTO_CREATE_DAYS):
+        if options['shift'] is not None:
+            shift = RecurringShift.objects.filter(pk=options['shift']).first()
+            if shift is None:
+                self.stdout.write(self.style.ERROR(f'Shift with pk {options["shift"]} does not exist.'))
+                return
+            self.create_shifts_for(shift, options['days'] or shift.auto_create_days)
+        for shift in RecurringShift.objects.all():
+            self.create_shifts_for(shift, shift.auto_create_days)
+
+    def create_shifts_for(self, shift: RecurringShift, days: int or None = None) -> None:
+        for i in range(days):
             day = date.today() + timedelta(days=i)
-            self.stdout.write(self.style.NOTICE(f'Creating for {day}.'))
-            for _shift in RecurringShift.objects.all():
-                shift: RecurringShift = _shift
-                if shift.time_frame_type.matches_day(shift, day):
-                    shift.create_shifts(day)
+            if shift.time_frame_type.matches_day(shift, day):
+                shift.create_shifts(day)
